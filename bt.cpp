42b6a122a53b796b7858020c690b459247015ce4
Pritesh Agarwal <pa5174@srmist.edu.in>
2024-03-30 18:24:42 +0530

    add GotoWP node and sim logic for 1st fallback node

--- bt.cpp
+++ bt.cpp
@@ -5,6 +5,7 @@
 
 using namespace std::chrono_literals;
 
+//AtWP
 class AtWP : public BT::SyncActionNode
 {
 	public:
@@ -13,14 +14,38 @@ class AtWP : public BT::SyncActionNode
 		BT::NodeStatus tick() override
 		{
 			std::cout<<"Checking if Reached at WayPoint: "<<this->name() <<std::endl;
-			return BT::NodeStatus::SUCCESS;
+			int status;std::cin>>status;
+			if(status){
+				std::cout<<"Rover already at WayPoint"<<std::endl;
+				return BT::NodeStatus::SUCCESS;
+			}
+			else{
+				return BT::NodeStatus::FAILURE;
+			}
 		}
 };
+
+//GotoWP
+class GotoWP : public BT::SyncActionNode
+{
+        public:
+                explicit GotoWP(const std::string &name) : BT::SyncActionNode(name, {})
+                        {}
+                BT::NodeStatus tick() override
+                {
+                        std::cout<<"Searching and Moving to WayPoint: "<<this->name() <<std::endl;
+                        return BT::NodeStatus::SUCCESS;
+                }
+};
+
+//TubeFound
+
 
 int main()
 {
 	BT::BehaviorTreeFactory factory;
 	factory.registerNodeType<AtWP>("AtWP");
+	factory.registerNodeType<GotoWP>("GotoWP");
 
 	auto tree = factory.createTreeFromFile("./../bt_tree.xml");
 
