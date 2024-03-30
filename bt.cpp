4f703d968e482a5e919bff9a9b2ae94a62c0325a
Pritesh Agarwal <pa5174@srmist.edu.in>
2024-03-30 18:07:46 +0530

    add AtWP conditon node and main bt factory

--- bt.cpp
+++ bt.cpp
@@ -4,3 +4,26 @@
 #include "behaviortree_cpp/bt_factory.h"
 
 using namespace std::chrono_literals;
+
+class AtWP : public BT::SyncActionNode
+{
+	public:
+		explicit AtWP(const std::string &name) : BT::SyncActionNode(name, {})
+			{}
+		BT::NodeStatus tick() override
+		{
+			std::cout<<"Checking if Reached at WayPoint: "<<this->name() <<std::endl;
+			return BT::NodeStatus::SUCCESS;
+		}
+};
+
+int main()
+{
+	BT::BehaviorTreeFactory factory;
+	factory.registerNodeType<AtWP>("AtWP");
+
+	auto tree = factory.createTreeFromFile("./../bt_tree.xml");
+
+	tree.tickWhileRunning();
+	return 0;
+}
